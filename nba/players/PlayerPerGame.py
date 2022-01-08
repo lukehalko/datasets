43e4d503c83ffe4a94e4a35c5ef78783447efd8d
lukehalko <halko@udel.edu>
2022-01-08 12:25:33 -0500

    script acquires urls to individual seasons

--- nba/players/PlayerPerGame.py
+++ nba/players/PlayerPerGame.py
@@ -8,5 +8,14 @@ browser = webdriver.Chrome("/Users/lukeh/Desktop/tmp/chromedriver")
 browser.get("https://www.basketball-reference.com/leagues/")
 time.sleep(1)
 
-tbody = browser.find_element_by_xpath(".//table[@id='stats']").find_element_by_tag_name("tbody")
+urls = []
+
+for i in range(86):
+    row = browser.find_element_by_xpath(f".//tr[@data-row={i}]")
+    if "thead" not in row.get_attribute("class"):
+        link = row.find_element_by_xpath(".//th[@data-stat='season']").find_element_by_tag_name("a").get_attribute("href")
+        urls.append(link)
+
+
+
 
