0cb199923ba7ca16abdbb9cc07a71e72de4897a1
lukehalko <halko@udel.edu>
2022-06-29 10:37:25 -0400

    fully acquired weather dataset

--- weather/phillyWeather.py
+++ weather/phillyWeather.py
@@ -13,7 +13,7 @@ months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", 
 
 data = {
     "date": [],
-    # "time": [],
+    "time": [],
     "temp": [],
     "weather": [],
     "wind": [],
@@ -24,8 +24,8 @@ data = {
 try:
     browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-    for year in range(2016, 2017):
-        for month in range(1, 2):
+    for year in range(2016, 2022):
+        for month in range(1, 13):
             print("entering new month")
             browser.get(f"https://www.timeanddate.com/weather/usa/philadelphia/historic?month={month}&year={year}")
 
@@ -33,41 +33,15 @@ try:
             for day in range(1,32):
                 try:
                     print(f"clicking {months[month-1]} {day}")
-                    try:
-                        links.find_element(By.PARTIAL_LINK_TEXT, f"{months[month-1]} {day}").click()
-                        print("got links")
-                    except Exception as e:
-                        print(e)
-                        print("ERROR GETTING LINKS, TRYING AGAIN...")
-                        links.find_element(By.PARTIAL_LINK_TEXT, f"{months[month-1]} {day}").click()
-                    try:
-                        rows = browser.find_element(By.XPATH, "//table[@id='wt-his']").find_element(By.TAG_NAME, "tbody").find_elements(By.TAG_NAME, "tr")
-                        print("got rows")
-                    except Exception as e:
-                        print(e)
-                        print("ERROR GETTING ROWS, TRYING AGAIN...")
-                        time.sleep(1)
-                        rows = browser.find_element(By.XPATH, "//table[@id='wt-his']").find_element(By.TAG_NAME, "tbody").find_elements(By.TAG_NAME, "tr")
+                    links.find_element(By.PARTIAL_LINK_TEXT, f"{months[month-1]} {day}").click()
+                    time.sleep(1)
+                    print("got links")
+                    rows = browser.find_element(By.XPATH, "//table[@id='wt-his']").find_element(By.TAG_NAME, "tbody").find_elements(By.TAG_NAME, "tr")
+                    print("got rows")
                     for row in rows:
                         data["date"].append(f"{month}-{day}-{year}")
-                        for i in range(1,5):
-                            try:
-                                #timestamp = row.find_element(By.TAG_NAME, "th").text
-                                x = 1
-                                break
-                            except Exception as e:
-                                print(e)
-                                print("ERROR GETTING TIME (th) ELEMENT, TRYING AGAIN...")
-                                time.sleep(1)
-                                # data["time"].append(row.find_element(By.TAG_NAME, "th").text)
-                        for i in range(1,5):
-                            try:
-                                values = row.find_elements(By.TAG_NAME, "td")
-                            except Exception as e:
-                                print(e)
-                                print("ERROR GETTING ROW VALUES, TRYING AGAIN...")
-                                time.sleep(1)
-                        # data["time"] = timestamp
+                        data["time"].append(row.find_element(By.TAG_NAME, "th").text)
+                        values = row.find_elements(By.TAG_NAME, "td")
                         data["temp"].append(values[1].text)
                         data["weather"].append(values[2].text)
                         data["wind"].append(values[3].text)
@@ -78,18 +52,14 @@ try:
                     print(f"Successfully collected for {months[month-1]} {day}")
                 except Exception as e:
                     print(e)
-
-    # df = pd.DataFrame.from_dict(data)
-    # print(df)
-
-    # df.to_csv("philadelphia_weather_2016_2022")
     print(data)
     for x in data:
         print(x + " " + str(len(data[x])))
 finally:
     try:
         df = pd.DataFrame.from_dict(data)
-        df.to_csv("philadelphia_weather_2016_2022")
+        df.to_csv("philadelphia_weather_2016_2021.csv")
     except Exception as e:
         print(e)
+        print("*** FAILED TO GENERATE CSV FILE ***")
     browser.quit()
